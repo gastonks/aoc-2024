ee2c081963a7033ec1deec65c4917f2de2eeaf27
gastonks <rombar999@gmail.com>
2024-12-03 13:45:15 +0100

    day 3 part 1

---
+++ day3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+var pattern = @"mul\(\d{1,},\d{1,}\)";
+int result = 0;
+int product = 1;
+while (Console.ReadLine() is { } line)
+{
+    MatchCollection matches = Regex.Matches(line, pattern);
+    
+    matches.ToList().ForEach(match =>
+    {
+        MatchCollection matchesNumber = Regex.Matches(match.Value, @"\d{1,}");
+        matchesNumber.ToList().ForEach(m =>
+        {
+            //Console.WriteLine(m);
+            product *= int.Parse(m.Value);
+        });
+        result += product;
+        product = 1;
+    });
+}
+
+Console.WriteLine(result);
